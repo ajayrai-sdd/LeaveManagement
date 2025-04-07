21749fe126621d7ddc883ba3eef740a42524cb4a
ajay.rai <ajay.rai@SDD-2>
2025-04-07 15:25:12 +0530

    authentication updates

---
+++ LeaveService/Models/LeaveRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveService.Models
+{
+    public class Leave
+    {
+        public int Id { get; set; }
+        public string EmployeeEmail { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; } = "Pending";
+    }
+    public class LeaveRequest
+    {
+        [EmailAddress]
+        public string EmployeeEmail { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
